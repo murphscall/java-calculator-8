93a0252197b58d408313b5867c60a9adb588ea6c
kimjinhoo <wlsgnwkd22@naver.com>
2025-10-15 06:53:13 +0900

    feat(addCustomDelimiter) : 커스텀 구분자 확인 및 추가 기능 구현 - 잘못된 형식의 구분자 입력 시 예외 발생 - 올바른 형식의 구분자 일 경우 기존 구분자에 추가하여 반환 - 커스텀 구분자가 없을 경우에는 기존 구분자 반환

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,11 @@
 package calculator;
 
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
-    private static final String DEFAULT_DELIMITER = "[,|:]";
+    private static final String DEFAULT_DELIMITER = ",|:";
 
     public int calc(String input) {
         // 입력 값이 null 이라면 예외를 던진다.
@@ -13,13 +15,28 @@ public class StringCalculator {
             return 0;
         }
 
+        String delimiter = addCustomDelimiter(input);
+
         return 1;
     }
 
-    private void validateInput(String input) {
+    public void validateInput(String input) {
         if (input == null) {
             throw new IllegalArgumentException("입력이 null 일 수 없습니다.");
         }
+    }
+
+    private String addCustomDelimiter(String input) {
+        if (!input.startsWith("//")) {
+            return DEFAULT_DELIMITER;
+        }
+        int idx = input.indexOf("\n");
+        if (idx == -1) {
+            throw new IllegalArgumentException("잘못된 구분자 형식 입니다.");
+        }
+
+        String customDelimiter = Pattern.quote(input.substring(2, idx));
+        return customDelimiter + "|" + DEFAULT_DELIMITER;
     }
 
 
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class StringCalculatorTest {
 
@@ -11,6 +13,7 @@ class StringCalculatorTest {
 
     @Test
     void 입력값이_null이면_예외를_반환한다() {
+
         String input = null;
 
         assertThrows(IllegalArgumentException.class, () -> stringCalculator.calc(input),
@@ -19,10 +22,29 @@ class StringCalculatorTest {
 
     @Test
     void 입력값이_빈문자열이면_0을_반환한다() {
+
         String input = "";
 
         int result = stringCalculator.calc(input);
 
         assertEquals(0, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;!n1;2;3", "//?!n1;2?3"})
+    void 올바른_커스텀구분자_형식이아니면_예외를_반환한다(String input) {
+        // calc() 호출 시 잘못된 커스텀 구분자 형식이면 예외 발생
+        assertThrows(IllegalArgumentException.class, () -> stringCalculator.calc(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2:3", "//?\n1?2:3"})
+    void 올바른_커스텀구분자_형식이면_커스텀구분자를_추가하여_반환한다() {
+
+        String input = "//;\n1;2:3";
+
+        int result = stringCalculator.calc(input);
+
+        assertEquals(1, result);
     }
 }
